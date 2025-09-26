e4fec6300cec4acd3986297c137bb2b4a0aef29d
JuanV24 <135783572+JuanV24@users.noreply.github.com>
2025-09-25 19:31:26 -0600

    Subiendo el ejercicio 5

--- Ejercicios/src/Ejercicio5/Ejercicio5.java
+++ Ejercicios/src/Ejercicio5/Ejercicio5.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Ejercicio5;
 
-/**
- *
- * @author USER
- */
+package Ejercicio5;
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Declarando variables y objetos
+        Scanner entrada = new Scanner(System.in);
+        int num, resultado = 1;
+        
+        
+        
+        //Pidiendo el valor
+        System.out.print("Ingrese el numero que desea saber su factorial: ");
+        num = entrada.nextInt();
+        
+        //Bucle el donde iteraremos i para que se multiplique con el resultado
+        for(int i = 1; i <= num; i++){
+            //Guardando el resultado y volviendo a multiplicarlo
+            resultado *= i ;
+            
+        }
+        
+        System.out.println("El facotorial es de "+num+" : "+ resultado);
+        
     }
     
 }
